99f736033b1704e1429398c0809ee1f7c6f6cc1c
tasuku43 <task.teckac@gmail.com>
2024-01-24 07:00:38 +0900

    pass 4st stage

--- app/main.go
+++ app/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"syscall"
 )
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
@@ -16,6 +19,12 @@ func main() {
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
+	err := setupChrootWithBinary(command)
+	if err != nil {
+		fmt.Println("error creating isolated env: ", err)
+		os.Exit(1)
+	}
+
 	if err := cmd.Run(); err != nil {
 		var exitError *exec.ExitError
 		if errors.As(err, &exitError) {
@@ -25,4 +34,54 @@ func main() {
 		}
 	}
 	os.Exit(0)
+}
+
+func setupChrootWithBinary(binPath string) error {
+	tempDir, err := os.MkdirTemp("", "isolated-root")
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		if err := os.RemoveAll(tempDir); err != nil {
+			fmt.Println("error removing temp dir: ", err)
+		}
+	}()
+
+	binDir := filepath.Dir(binPath)
+	execPath := filepath.Join(tempDir, binDir)
+	if err := os.MkdirAll(execPath, 0755); err != nil {
+		return err
+	}
+
+	binName := filepath.Base(binPath)
+	isolatedBinPath := filepath.Join(execPath, binName)
+	if err := os.Link(binPath, isolatedBinPath); err != nil {
+		return fmt.Errorf("error linking %s: %w", binName, err)
+	}
+
+	if err := syscall.Chroot(tempDir); err != nil {
+		return err
+	}
+
+	if err := os.Chdir("/"); err != nil {
+		return err
+	}
+
+	if err := os.Mkdir("/dev", 0755); err != nil {
+		return err
+	}
+
+	devNull, err := os.Create("/dev/null")
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		if err := devNull.Close(); err != nil {
+			fmt.Println("error closing dev null: ", err)
+		}
+	}()
+
+	return nil
 }
